6fd9dba646eebac7aece699d61edfa5e16ebc4ac
Golub Andrey <golubandrey31@gmail.com>
2018-10-18 22:58:32 +0300

    Day 4 - Number transform to words

---
+++ Day 4/NumberTransformToWords/NUnitNumberToWordsTransformerTests/NUnitNumberToWordsTransformerTester.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using NUnit.Framework;
+using NumberTransformToWords;
+
+namespace NUnitNumberToWordsTransformerTests
+{
+    [TestFixture]
+    public class NUnitNumberToWordsTransformerTester
+    {
+        [TestCase(new double[3] { -23.809, 0.295, 15.17 },new string[3] { "minus two three point eight zero nine", "zero point two nine five", "one five point one seven" })]
+        [TestCase(new double[1] { 12.855},new string[1] {"one two point eight five five"})]
+        public void TransformToWords_ValidDoubleArray(double[] array,string[] expectedResult)
+        {
+            string[] result = NumberToWordsTransformer.TransformToWords(array);
+
+            CollectionAssert.AreEqual(expectedResult, result);
+        }
+
+        [TestCase(null)]
+        public void TransformToWords_NullDoubleArray(double[] array)
+        {
+            Assert.Throws<ArgumentNullException>(() => NumberToWordsTransformer.TransformToWords(array));
+        }
+
+        [TestCase(new double[0])]
+        public void TransformToWords_EmptyDoubleArray(double[] array)
+        {
+            Assert.Throws<ArgumentException>(() => NumberToWordsTransformer.TransformToWords(array));
+        }
+    }
+}
---
+++ Day 4/NumberTransformToWords/NumberTransformToWords/NumberToWordsTransformer.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NumberTransformToWords
+{
+    public static class NumberToWordsTransformer
+    {
+        private static string[] words = new string[10] { "zero", "one", "two", "three", "fouth", "five", "six", "seven", "eight", "nine" };
+
+        /// <summary>
+        /// Transform double array into a string array of words
+        /// </summary>
+        /// <param name="numberArray">Input double array</param>
+        /// <returns>String array of words</returns>
+        public static string[] TransformToWords(double[] numberArray)
+        {
+            if (numberArray==null)
+            {
+                throw new ArgumentNullException();
+            }
+            if (numberArray.Length==0)
+            {
+                throw new ArgumentException();
+            }
+
+            string[] resultArray = new string[numberArray.Length];
+
+            for (int i=0;i<numberArray.Length;i++)
+            {
+                resultArray[i] = DoubleToWords(numberArray[i]);
+            }
+
+            return resultArray;
+
+        }
+
+        private static string DoubleToWords(double number)
+        {
+            string stringNumber = number.ToString();
+            char[] numberArray = stringNumber.ToArray();
+            string[] resultArray = new string[numberArray.Length];
+
+            for (int i=0;i<numberArray.Length;i++)
+            {
+                if (numberArray[i]==',' || numberArray[i] == '.')
+                {
+                    resultArray[i] = String.Copy("point");
+                    continue;
+                }
+                if (numberArray[i] == '-')
+                {
+                    resultArray[i] = string.Copy("minus");
+                    continue;
+                }
+                resultArray[i]=String.Copy(words[Convert.ToInt32(numberArray[i] - 48)]);
+            }
+            string result=String.Join(" ", resultArray);
+            
+
+            return result;
+        }
+
+    }
+}
