f6a5a1bc91abf134cdde7bdc4d69241e3f6b2e22
Golub Andrey <golubandrey31@gmail.com>
2018-11-15 01:28:57 +0300

    Final version of Queue in Day 15

--- Day 15/Queue/Queue/Queue.cs
+++ Day 15/Queue/Queue/Queue.cs
@@ -6,9 +6,9 @@ using System.Threading.Tasks;
 
 namespace Queue
 {
-    public class QueueRealisation<T>:IEnumerable<T>
+    public class QueueRealisation<T> : IEnumerable<T>
     {
-        private const int sizeCoefficientMultiplier= 2;
+        private const int sizeCoefficientMultiplier = 2;
 
         private T[] elements;
         private int front;
@@ -16,33 +16,89 @@ namespace Queue
         private int size;
         private int count;
         private int index;
-        public int version { get; private set;}
+        public int version { get; private set; }
 
+        /// <summary>
+        /// Create queue
+        /// </summary>
+        /// <exception cref="ArgumentException">Arise when input parameter = 0</exception>
+        /// <param name="startSize">Start queue size</param>
         public QueueRealisation(byte startSize)
         {
+            if (startSize == 0)
+            {
+                throw new ArgumentException($"{nameof(startSize)} can't be 0");
+            }
             front = 0;
             rear = -1;
             size = startSize;
             count = 0;
             version = 0;
             index = 0;
+            elements = new T[startSize];
         }
 
+        /// <summary>
+        /// Create queue from the collection
+        /// </summary>
+        /// <exception cref="ArgumentNullException">Thrown when input parameter is null</exception>
+        /// <param name="list">Collection for the queue</param>
+        public QueueRealisation(IEnumerable<T> list)
+        {
+            if (ReferenceEquals(list,null))
+            {
+                throw new ArgumentNullException($"{nameof(list)} can't be null");
+            }
+
+            T[] tempArray = list.ToArray();
+
+            if (tempArray.Length==0)
+            {
+                size = 1;
+                elements = new T[size];
+                count = 0;
+                rear = -1;
+            }
+            else
+            {
+                size = tempArray.Length;
+                elements = new T[size];
+                tempArray.CopyTo(elements, 0);
+                rear = size - 1;
+                count = size;
+            }
+            front = 0;
+            version = 0;
+            index = 0;
+
+
+        }
+
+        /// <summary>
+        /// Adding element in the queue
+        /// </summary>
+        /// <param name="element">Element for the queue</param>
         public void Insert(T element)
         {
-            if (count==size)
+            if (count == size)
             {
-                Resize();
+                elements=Resize();
             }
             rear = (rear + 1) % size;
             elements[rear] = element;
             version++;
             count++;
         }
+        
 
+        /// <summary>
+        /// Leaving item from queue
+        /// </summary>
+        /// <exception cref="InvalidOperationException">Thrown when number of elements in queue equals 0</exception>
+        /// <returns>The item that left the queue </returns>
         public T Delete()
         {
-            if (count==0)
+            if (count == 0)
             {
                 throw new InvalidOperationException("Unable to remove item from empty queue");
             }
@@ -54,22 +110,62 @@ namespace Queue
             return tempStorage;
         }
 
+        /// <summary>
+        /// Take the first element in the queue
+        /// </summary>
+        /// <exception cref="InvalidOperationException">Throws when the queue is empty</exception>
+        /// <returns>First element</returns>
+        public T Peek()
+        {
+            if (IsEmpty())
+            {
+                throw new InvalidOperationException($"Queue can't be empty");
+            }
+            return elements[front];
+        }
+
+        /// <summary>
+        /// Check for empty queue
+        /// </summary>
+        /// <returns>true when queue is empty and false when queue has 1 or more element</returns>
+        public bool IsEmpty()
+        {
+            return count == 0 ? true : false;
+        }
+
+        /// <summary>
+        /// Check for full queue
+        /// </summary>
+        /// <returns>true when queue is full and false when queue has 1 or more free place</returns>
+        public bool isFull()
+        {
+            return count == size ? true : false;
+        }
+
+        /// <summary>
+        /// Get number of elements in the queue
+        /// </summary>
+        /// <returns>Number of elements in the queue</returns>
         public int Count() => count;
 
+        /// <summary>
+        /// Get iterator for the queue
+        /// </summary>
+        /// <returns>Object of IEnumerator</returns>
         public IEnumerator<T> GetEnumerator() => new QueueIterator(this);
 
         System.Collections.IEnumerator System.Collections.IEnumerable.GetEnumerator()
         {
             throw new NotImplementedException();
         }
-        
+
 
         internal T GetElement(int index)
         {
             return elements[(front + index) % count];
         }
-        
-        private void Resize()
+
+        private T[] Resize()
         {
             T[] newArray = new T[size * sizeCoefficientMultiplier];
             Array.Copy(elements, front, newArray, 0, size - front);
@@ -77,6 +173,7 @@ namespace Queue
             size *= sizeCoefficientMultiplier;
             front = 0;
             rear = count - 1;
+            return newArray;
         }
 
         private struct QueueIterator:IEnumerator<T>
---
+++ Day 15/Queue/QueueTests/QueueTester.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using Queue;
+
+namespace QueueTests
+{
+    [TestFixture]
+    public class QueueTester
+    {
+        [Test]
+        public void QueueTest_InvalidCreatingQueue() =>Assert.Throws<ArgumentException>(() 
+            => new QueueRealisation<int>(0));
+        
+        [Test]
+        public void QueueTest_Adding3Elements_QueueHas3Element()
+        {
+            QueueRealisation<int> qeue = new QueueRealisation<int>(1);
+
+            qeue.Insert(1);
+            qeue.Insert(3);
+            qeue.Insert(3);
+
+            Assert.AreEqual(3, qeue.Count());
+        }
+
+        [Test]
+        public void QueueTest_CreatingQueueWith3Elements_QueueHas3Element()
+        {
+            QueueRealisation<string> qeue = new QueueRealisation<string>(new List<string> { "asd", "asd","aaa" });
+            
+            Assert.AreEqual(3, qeue.Count());
+        }
+
+        [Test]
+        public void QueueTest_Add2ElementsAndTake1_QueueHas1Element()
+        {
+            QueueRealisation<string> queue = new QueueRealisation<string>(new List<string> { "asd", "asd" });
+
+            queue.Delete();
+
+            Assert.AreEqual(1, queue.Count());
+        }
+
+        [Test]
+        public void QueueTest_QueueWith1ElementAndDelete2_InvalideOperationException()
+        {
+            QueueRealisation<string> queue = new QueueRealisation<string>(1);
+            queue.Insert("aa");
+            queue.Delete();
+
+            Assert.Throws<InvalidOperationException>(()=>queue.Delete());
+        }
+
+        [Test]
+        public void GetQueueEnumerator_TwoStepsAhead_GetCurrentElement_CurrentElementEqual2AddingElementInQueue()
+        {
+            QueueRealisation<string> queue = new QueueRealisation<string>(new List<string> { "a", "ab", "abc","abcd" });
+
+            var enumerator = queue.GetEnumerator();
+
+            enumerator.MoveNext();
+            enumerator.MoveNext();
+
+            Assert.AreEqual("ab", enumerator.Current);
+        }
+            
+    }
+}
