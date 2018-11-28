b0e8bbbc3efd1ce72bea888d2ebaff7faa12d324
Golub Andrey <golubandrey31@gmail.com>
2018-11-28 17:51:50 +0300

    Tests in console application

---
+++ Day 19/URLSerializer/URLTester/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using URLSerializer;
+using Ninject;
+
+namespace URLTester
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IKernel kernel = new StandardKernel(new ConfigModule());
+            Process<string, URL> process = kernel.Get<Process<string, URL>>();
+            process.Start(@"E:\EPAM\NET.A.2018.Golub\Day 19\URLSerializer\xml.xml");
+            Console.WriteLine("!");
+            Console.ReadLine();
+        }
+    }
+}
